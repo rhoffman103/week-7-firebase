c4f46f41ba7abdbd5a5d7093cab8bdadd2b63b76
Bobby H <rhoffman103@gmail.com>
2018-07-24 21:13:19 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# week-7-firebase
+# Train SChedule
+Week 7 assignment. Use firebase to host arrival and departure data in a train scheduling application. Administrators will add and update train names, times, destination, frequency and first train time.
+
+## Technologies used
+- jQuery
+- Bootstrap
+- Firebase
+
+## Created By
+Bobby Hoffman
---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxnWcXiTEFpdGs3P5Th2VWFhtBWqWQw2k",
+    authDomain: "train-schedule-d0387.firebaseapp.com",
+    databaseURL: "https://train-schedule-d0387.firebaseio.com",
+    projectId: "train-schedule-d0387",
+    storageBucket: "",
+    messagingSenderId: "595890089480"
+  };
+  firebase.initializeApp(config);
