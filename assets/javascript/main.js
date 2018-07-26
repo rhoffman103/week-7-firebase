d4ff6722250ce4ef54ba7f64ae4692fddc15d18a
Bobby H <rhoffman103@gmail.com>
2018-07-26 10:34:52 -0400

    firebase push and append to html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBxnWcXiTEFpdGs3P5Th2VWFhtBWqWQw2k",
@@ -8,3 +10,50 @@
     messagingSenderId: "595890089480"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
+  // Initial Values
+  var name         = "";
+  var destination  = "";
+  var firstRunTime = "";   // hh:mm
+  var frequency    = "";
+  var nextArrival  = "";
+  var minutesAway  = "";
+  
+  // APPEND TRAIN
+  const appendTrain = function(snap) {
+    var trainDiv = $("<div>").addClass("row p-2 table-row")
+    // var p = $("<p>").addClass("card-text col")
+    console.log(snap);
+    $("#trainTable").append(trainDiv);
+    trainDiv.append($("<p>").addClass("card-text col").text(snap.name))
+      .append($("<p>").addClass("card-text col").text(snap.destination))
+      .append($("<p>").addClass("card-text col").text(snap.frequency))
+      .append($("<p>").addClass("card-text col").text(nextArrival))
+      .append($("<p>").addClass("card-text col").text(minutesAway));
+  }
+  
+  // Events
+  $(".submit-btn").on("click", function() {
+    name = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    firstRunTime = $("#firstTrainTime").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    // Push to firebase
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstRunTime: firstRunTime,
+      frequency: frequency
+    });
+  });
+
+  // Firebase watcher
+  database.ref().on("child_added", function(childSnapshot) {
+    appendTrain(childSnapshot.val());
+  });
+  
+
+});
